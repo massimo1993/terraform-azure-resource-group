72ce4c19894ab8c6c8b9017575ebca0a2fc1e4f7
Massimo Cannavo <massimo5cannavo@gmail.com>
2020-11-09 19:28:41 -0700

    Refactored name of resource

--- resource-group.tf
+++ resource-group.tf
@@ -1,15 +1,13 @@
-locals {
-  env = substr(var.info.environment, 0, 1)
-
-  name   = replace(module.naming.resource_group.name, "-", "")
-  length = module.naming.resource_group.max_length - 4
-
-  prefix = substr(local.name, 0, local.length)
-}
-
 resource azurerm_resource_group resource_group {
-  name     = format("%s%s%03d", local.prefix, local.env, var.info.sequence)
-  location = var.region
+  name = format("%s%s%03d",
+    substr(
+      replace(module.naming.resource_group.name, "-", ""),
+      module.naming.resource_group.max_length - 4
+    ),
+    substr(var.info.environment, 0, 1),
+    var.info.sequence
+  )
 
-  tags = local.tags
+  location = var.region
+  tags     = local.tags
 }
