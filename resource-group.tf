7f90b43cd357fd102f3c2f3f0e63ffaaaa80146e
Massimo Cannavo <massimo5cannavo@gmail.com>
2020-11-10 20:45:55 -0700

    Refactored naming convention

--- resource-group.tf
+++ resource-group.tf
@@ -2,9 +2,9 @@ resource azurerm_resource_group resource_group {
   name = format("%s-%s-%03d",
     substr(
       module.naming.resource_group.name, 0,
-      module.naming.resource_group.max_length - 4
+      module.naming.resource_group.max_length - 6
     ),
-    local.environment,
+    substr(local.environment, 0, 3),
     var.info.sequence
   )
 
