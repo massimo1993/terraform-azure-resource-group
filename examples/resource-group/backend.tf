a5446ea91cc7cef1747eae44aab1ec2f3ff80fe6
Massimo Cannavo <massimo1993@pm.me>
2021-06-05 18:44:43 -0700

    Updated backend

--- examples/resource-group/backend.tf
+++ examples/resource-group/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-common-d001"
-    storage_account_name = "stcommond001"
+    resource_group_name  = "rg-common-dev-001"
+    storage_account_name = "stcommondev001"
     container_name       = "terraform"
     key                  = "resource-group.tfstate"
     snapshot             = true
